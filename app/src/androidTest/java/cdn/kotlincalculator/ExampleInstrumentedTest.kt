57d644e66e86c418aa3647796ce82ca1622258b6
Ripunjaya Saran <ripunjaya@noom.com>
2021-08-06 15:22:44 -0400

    Revert "minor changes"
    This reverts commit 9d4dbe068d1f73550c86cd20b9266f6da0d361bc.

---
+++ app/src/androidTest/java/cdn/kotlincalculator/ExampleInstrumentedTest.kt
@@ -0,0 +1,22 @@
+package cdn.kotlincalculator
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+
+    @Test
+    fun testCalculator() {
+
+    }
+}
