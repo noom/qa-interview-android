3b16ab3fe2ca0d9ca209c66dbde5b0d59fc719dd
Ripunjaya Saran <ripunjaya@noom.com>
2021-08-09 15:17:09 -0400

    One more change

--- app/src/androidTest/java/cdn/kotlincalculator/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/cdn/kotlincalculator/ExampleInstrumentedTest.kt
@@ -2,6 +2,7 @@ package cdn.kotlincalculator
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -14,6 +15,10 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    var activityRule: ActivityScenarioRule<MainActivity>
+            = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
     fun testCalculator() {
